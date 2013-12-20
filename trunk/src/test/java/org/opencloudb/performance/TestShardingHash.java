19e74524fb45f75d0b352b308fe5154d9c03cdbe
shenzhw <shenzhw@333ad601-27c3-4164-9dfd-7d68b599a590>
2013-12-20 08:49:46 +0000

    按范围分片代码已提交

--- trunk/src/test/java/org/opencloudb/performance/TestShardingHash.java
+++ trunk/src/test/java/org/opencloudb/performance/TestShardingHash.java
@@ -1,5 +1,7 @@
 package org.opencloudb.performance;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -7,7 +9,7 @@ import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import org.opencloudb.performance.job.InsertJob;
+import org.opencloudb.performance.job.CustomerJob;
 
 /**
  * 测试按哈希分片功能
@@ -28,13 +30,31 @@ public class TestShardingHash {
 	}
 	
 	public static void main(String[] args) throws Exception{
-		ExecutorService pool = Executors.newFixedThreadPool(100);
+		int threadCount = 0;//线程数
+		int recordCount = 0;//要插入的记录数
+		try{
+			threadCount = Integer.parseInt(args[0]);
+			recordCount = Integer.parseInt(args[1]);
+		}catch(Exception e){
+			System.out.println("请输入正确的参数,格式为:[线程池大小] [记录数]");
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			try {
+				String params = br.readLine();
+				threadCount = Integer.parseInt(params.split(" ")[0]);
+				recordCount = Integer.parseInt(params.split(" ")[1]);
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		System.out.println("线程池大小:" + threadCount);
+		System.out.println("插入记录数:" + recordCount);
+		
+		ExecutorService pool = Executors.newFixedThreadPool(threadCount);
 		
 		long start = System.currentTimeMillis();
 		List<Map> list = new ArrayList<Map>();
 		int c = 0;
-		int sumcount = 100*10000;
-		for(int i=0;i<sumcount;i++){
+		for(int i=0;i<recordCount;i++){
 			Map m = new HashMap();
 			m.put("id", i);
 			m.put("name", "customer" + i);
@@ -51,7 +71,7 @@ public class TestShardingHash {
 			list.add(m);
 			c += 1;
 			if(c==5000){
-				InsertJob job = new InsertJob();
+				CustomerJob job = new CustomerJob();
 				job.setList(list);
 				pool.execute(job);
 				c=0;
@@ -59,7 +79,7 @@ public class TestShardingHash {
 			}
 		}
 		System.out.println("已分配完任务，耗时:" + (System.currentTimeMillis() - start)/1000 + "秒");
-		while(count < sumcount){
+		while(count < recordCount){
 			System.out.println("已插入记录条数:" + count);
 			Thread.sleep(1000);
 		}
